9e8fcd39f21386bb4274d1d7cd437f014be29e2e
Monique Ferraz <mo_ferraz@hotmail.com>
2024-01-27 18:18:59 -0300

    Adicionar arquivos de projeto.

---
+++ lista_nome_idade/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_nome_idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[2];
+            int[] idades = new int[2];
+
+            for (int indice = 0; indice < 2; indice++)
+            {
+                Console.WriteLine("Digite o nome do Aluno");
+                nomes[indice] = Console.ReadLine();
+
+                Console.WriteLine("Digite a idade do aluno");
+                idades[indice] = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Lista de alunos");
+
+            for (int indice = 0;indice < 2; indice++)
+            {
+                Console.WriteLine($" Nome: {nomes[indice]} idade: {idades[indice]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
